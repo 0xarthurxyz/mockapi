acc12545f41b6ee38c4fc366f04e9e1d3f65b071
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-05-31 22:40:24 +0100

    feat(notify): adds stub `sendNotification` method

---
+++ notify.js
@@ -0,0 +1,10 @@
+
+// Stub function for sending notifications
+function sendNotification(eventID, notification) {
+    console.log(`Stub notification sent for 
+        \neventID: ${eventID}
+        \nwith notification: ${notification}`)
+}
+
+// Exports function
+module.exports = sendNotification
